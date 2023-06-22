d047d3d0de68920656820e1f4685d69fe7620b95
rajandangi <rajandangi649@gmail.com>
2023-06-22 22:43:27 +0930

    [likebox] Enforce Limit of one Like per User combo

--- wp-content/themes/fictional-university-theme/inc/like-route.php
+++ wp-content/themes/fictional-university-theme/inc/like-route.php
@@ -21,17 +21,40 @@ function universityLikeRoutes()
 }
 function createLike($data)
 {
-    $professor = sanitize_text_field($data['professorId']);
-    wp_insert_post(
-        array(
-            'post_type' => 'like',
-            'post_status' => 'publish',
-            'post_title' => '2 Our PHP Create Post',
-            'meta_input' => array(
-                'liked_professor_id' => $professor
+    if (is_user_logged_in()) {
+        $professor = sanitize_text_field($data['professorId']);
+
+        $existQuery = new WP_Query(
+            array(
+                'author' => get_current_user_id(),
+                'post_type' => 'like',
+                'meta_query' => array(
+                    array(
+                        'key' => 'liked_professor_id',
+                        'compare' => '=',
+                        'value' => $professor
+                    )
+                ),
             )
-        )
-    );
+        );
+
+        if ($existQuery->found_posts == 0 and get_post_type($professor) == 'professor') {
+            return wp_insert_post(
+                array(
+                    'post_type' => 'like',
+                    'post_status' => 'publish',
+                    'post_title' => '2nd PHP create post test.',
+                    'meta_input' => array(
+                        'liked_professor_id' => $professor
+                    )
+                )
+            );
+        } else {
+            die("Invalid professor id.");
+        }
+    } else {
+        die("Only logged in users can create a like.");
+    }
 }
 function deleteLike()
 {

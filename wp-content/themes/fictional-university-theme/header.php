0cab7c6532f1212633421e1031640b69502be400
rajandangi <rajandangi649@gmail.com>
2024-01-04 17:31:22 +1030

    Add "My Notes" link in header and page

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -31,6 +31,8 @@
                 <div class="site-header__util">
                     <?php
                     if (is_user_logged_in()) { ?>
+                        <a href="<?php echo esc_url(site_url('/my-notes')); ?>"
+                            class="btn btn--small btn--orange float-left push-right">My Notes</a>
                         <a href="<?php echo wp_logout_url(); ?>"
                             class="btn btn--small btn--dark-orange float-left btn--with-photo">
                             <span class="site-header__avatar">
@@ -39,6 +41,7 @@
                             <span class="btn__text">Log Out</span>
                         </a>
                     <?php } else { ?>
+
                         <a href="<?php echo wp_login_url(); ?>"
                             class="btn btn--small btn--orange float-left push-right">Login</a>
                         <a href="<?php echo wp_registration_url(); ?>"
